f55673d58f7c737469db45095d7ad3cf94f5f9be
josephgijonmedel-pixel <josephgijonmedel@gmail.com>
2026-04-12 17:08:24 -0600

    Fix Views

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -12,6 +12,11 @@ public partial class MainPage : ContentPage
         // Le decimos a la Vista que su "cerebro" es el MainViewModel
         BindingContext = new ViewModels.MainViewModels();
     }
+    private void OnConectarClicked(object sender, EventArgs e)
+    {
+        // Aquí puedes poner un mensaje o llamar a la conexión
+        DisplayAlert("Conexión", "Intentando conectar al bastón...", "OK");
+    }
 
-    
+
 }
